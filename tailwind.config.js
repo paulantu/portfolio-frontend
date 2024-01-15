f45d7ae2cfafe74219ea280c67b7be6c82e46337
Antu Paul <antupaul81@gmail.com>
2024-01-15 17:38:38 +0600

    Initial frontend design

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 7s linear infinite',
+      }
+    },
+  },
+  plugins: [],
+}
+
